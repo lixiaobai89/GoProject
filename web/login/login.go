db3fd68fd9bba2635d8afd51e3db7ab9e51fc06b
lxb <263100476@qq.com>
2020-06-14 19:46:38 +0800

    增加index请求

--- web/login/login.go
+++ web/login/login.go
@@ -10,6 +10,7 @@ import (
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("以下打印sayhelloName请求的信息")
 	r.ParseForm() //解析url传递的参数，对于POST则解析响应包的主体（request body）
 	//注意:如果没有调用ParseForm方法，下面无法获取表单的数据
 	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
@@ -24,8 +25,10 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("以下打印login请求的信息")
 	fmt.Println("method:", r.Method) //获取请求的方法
 	if r.Method == "GET" {
+		fmt.Println("login success")
 		t, _ := template.ParseFiles("login.html")
 		log.Println(t.Execute(w, nil))
 	} else {
@@ -45,10 +48,18 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("以下打印index请求的信息")
+	if r.Method == "GET" {
+		template.ParseFiles("index.html")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
-	http.HandleFunc("/login", login)         //设置访问的路由
-	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	http.HandleFunc("/", index)                    //设置访问的路由
+	http.HandleFunc("/sayhelloName", sayhelloName) //设置访问的路由
+	http.HandleFunc("/login", login)               //设置访问的路由
+	err := http.ListenAndServe(":9090", nil)       //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
